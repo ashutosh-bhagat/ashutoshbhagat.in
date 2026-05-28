ab680cbbe3b8e87ecb2681de87496b5194eefdf8
Ashutosh <23csd25@svitvasad.ac.in>
2026-05-28 15:00:56 +0530

    Dynamic Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -8,6 +9,7 @@ import Contact from "./components/Contact";
 export default function App() {
   return (
     <>
+      <CustomCursor />
       <Navbar />
       <Hero />
       <About />
